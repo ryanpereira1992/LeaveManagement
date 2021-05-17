091bf8941d527c6778e3609daa91d52131712dda
Ryan Pereira <ryan_pereira_92@hotmail.com>
2021-05-17 17:50:00 +0530

    Completed CRUD for Leave Types

--- LeaveManagement/Controllers/LeaveTypeController.cs
+++ LeaveManagement/Controllers/LeaveTypeController.cs
@@ -125,21 +125,47 @@ namespace LeaveManagement.Controllers
         // GET: LeaveTypeController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leaveType = _repo.FindById(id);
+
+            if (leaveType == null)
+            {
+                return NotFound();
+            }
+
+            var isSuccess = _repo.Delete(leaveType);
+
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypeController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveTypeVM model)
         {
             try
             {
+                var leaveType = _repo.FindById(id);
+
+                if (leaveType == null)
+                {
+                    return NotFound();
+                }
+
+                var isSuccess = _repo.Delete(leaveType);
+
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }
